5424f9cbec0ef6e4d17364411d0155ca3f8011c8
morgan <morgantdarck@gmail.com>
2025-02-10 23:38:43 +0300

    * 10.02.2025
    Начата верстка страницы "События"

--- src/main/java/ru/fcpsr/portaldomainsport/controllers/web/HomeController.java
+++ src/main/java/ru/fcpsr/portaldomainsport/controllers/web/HomeController.java
@@ -1,17 +1,15 @@
 package ru.fcpsr.portaldomainsport.controllers.web;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class HomeController {
-    private static final Logger log = LoggerFactory.getLogger(HomeController.class);
     @GetMapping("/")
     public String home(Model model) {
-        log.info("Home page");
         model.addAttribute("title", "Главная");
         model.addAttribute("page","home");
         return "index";
