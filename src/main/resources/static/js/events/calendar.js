a98a157301a7d4450fc8fd279506d9f3aa8c71d2
morgan <morgantdarck@gmail.com>
2025-02-13 23:19:52 +0300

    * 13.02.2025 - доработка календаря (анимации)

--- src/main/resources/static/js/events/calendar.js
+++ src/main/resources/static/js/events/calendar.js
@@ -16,12 +16,19 @@ function day(date){
     return `
         <div class="col-12 col-md-6 col-lg-3 flip-card m-0 p-0" style="height: 300px">
             <div class="flip-card-inner position-relative w-100 h-100">
-                <div class="flip-card-front position-absolute d-flex flex-column">
-                    <p class="mx-auto mt-auto display-1">${date.day}</p>
+                <div class="flip-card-front position-absolute d-flex flex-column ${date.current ? 'border border-2 border-danger' : ''}">
+                    <p class="mx-auto mt-auto display-1 ${date.dayString === 'суббота' ? 'text-danger' : ''} ${date.dayString === 'воскресенье' ? 'text-danger' : ''}"><b>${date.day}</b></p>
                     <p class="mx-auto mb-auto fs-4">${date.dayString}</p>
                 </div>
-                <div class="flip-card-back position-absolute d-flex flex-column">
-                    <p class="m-auto fs-3">${date.day}.${date.month}.${date.year}</p>
+                <div class="flip-card-back position-absolute ${date.current ? 'border border-2 border-danger' : ''}">
+                    <div class="row h-100 position-relative">
+                        <div class="col-12 w-100 h-100">
+                            <img class="img-fluid object-fit-cover w-100 h-100" src="/img/tennis-tiger.jpeg" alt="image">
+                        </div>
+                        <div class="col-12 d-flex position-absolute bottom-0">
+                            <p class="mx-auto mb-auto fs-5 text-white"><b>${date.day}.${date.month}.${date.year}</b></p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -45,7 +52,8 @@ function getCurrentMonth() {
             dayString: dayString,
             month: month + 1, // Месяцы в JavaScript начинаются с 0, поэтому добавляем 1
             stringMonth: stringMonth,
-            year: year
+            year: year,
+            current: today.getDate() === day
         });
     }
 
